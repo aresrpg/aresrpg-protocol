8200625eeed10c28ba531a7f8028f8d11b09e581
Sceat <fetch@sceat.xyz>
2024-08-09 15:46:58 +0900

    feat: include types

---
+++ types/generated/ares_pb.d.ts
@@ -0,0 +1,127 @@
+/**
+ * Describes the file ares.proto.
+ */
+export const file_ares: import("@bufbuild/protobuf").DescFile;
+/**
+ * Describes the message ares.Packet.
+ * Use `create(PacketSchema)` to create a new message.
+ */
+export const PacketSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.SignatureRequest.
+ * Use `create(SignatureRequestSchema)` to create a new message.
+ */
+export const SignatureRequestSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.Error.
+ * Use `create(ErrorSchema)` to create a new message.
+ */
+export const ErrorSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.EntityGroupSpawn.
+ * Use `create(EntityGroupSpawnSchema)` to create a new message.
+ */
+export const EntityGroupSpawnSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.CharactersDespawn.
+ * Use `create(CharactersDespawnSchema)` to create a new message.
+ */
+export const CharactersDespawnSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.EntityGroupsDespawn.
+ * Use `create(EntityGroupsDespawnSchema)` to create a new message.
+ */
+export const EntityGroupsDespawnSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.FightsDespawn.
+ * Use `create(FightsDespawnSchema)` to create a new message.
+ */
+export const FightsDespawnSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.ServerInfo.
+ * Use `create(ServerInfoSchema)` to create a new message.
+ */
+export const ServerInfoSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.CharacterHealth.
+ * Use `create(CharacterHealthSchema)` to create a new message.
+ */
+export const CharacterHealthSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.FightSpawn.
+ * Use `create(FightSpawnSchema)` to create a new message.
+ */
+export const FightSpawnSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.SignatureResponse.
+ * Use `create(SignatureResponseSchema)` to create a new message.
+ */
+export const SignatureResponseSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.CharacterAttackMobGroup.
+ * Use `create(CharacterAttackMobGroupSchema)` to create a new message.
+ */
+export const CharacterAttackMobGroupSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.CharacterAction.
+ * Use `create(CharacterActionSchema)` to create a new message.
+ */
+export const CharacterActionSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.CharacterPosition.
+ * Use `create(CharacterPositionSchema)` to create a new message.
+ */
+export const CharacterPositionSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.ChatMessage.
+ * Use `create(ChatMessageSchema)` to create a new message.
+ */
+export const ChatMessageSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.RequestResponse.
+ * Use `create(RequestResponseSchema)` to create a new message.
+ */
+export const RequestResponseSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.Position.
+ * Use `create(PositionSchema)` to create a new message.
+ */
+export const PositionSchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the message ares.Entity.
+ * Use `create(EntitySchema)` to create a new message.
+ */
+export const EntitySchema: import("@bufbuild/protobuf/codegenv1").GenMessage<import("@bufbuild/protobuf").Message, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * Describes the enum ares.Classe.
+ */
+export const ClasseSchema: import("@bufbuild/protobuf/codegenv1").GenEnum<number, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * @generated from enum ares.Classe
+ */
+export const Classe: {
+    [key: number]: string;
+    [k: string]: string | number;
+};
+/**
+ * Describes the enum ares.ErrorCode.
+ */
+export const ErrorCodeSchema: import("@bufbuild/protobuf/codegenv1").GenEnum<number, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * @generated from enum ares.ErrorCode
+ */
+export const ErrorCode: {
+    [key: number]: string;
+    [k: string]: string | number;
+};
+/**
+ * Describes the enum ares.Action.
+ */
+export const ActionSchema: import("@bufbuild/protobuf/codegenv1").GenEnum<number, import("@bufbuild/protobuf").JsonValue>;
+/**
+ * @generated from enum ares.Action
+ */
+export const Action: {
+    [key: number]: string;
+    [k: string]: string | number;
+};
